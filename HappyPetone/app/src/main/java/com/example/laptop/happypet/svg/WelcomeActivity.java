cf803a4083eae0030c6ed6b866a9dd3efbb73689
github <18813049501@163.com>
2018-01-09 09:23:13 +0800

    侧滑+登录注册

---
+++ HappyPetone/app/src/main/java/com/example/laptop/happypet/svg/WelcomeActivity.java
@@ -0,0 +1,76 @@
+package com.example.laptop.happypet.svg;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.eftimoff.androipathview.PathView;
+import com.example.laptop.happypet.R;
+import com.example.laptop.happypet.guide.GuideActivity;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by Laptop on 2018/1/5.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    @InjectView(R.id.pathView)
+    PathView pathView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_main);
+
+        ButterKnife.inject(this);
+
+        initView();
+    }
+
+    private void initView() {
+        pathView.getPathAnimator()
+                .delay(1000)
+                .duration(3000)
+                .listenerStart(new PathView.AnimatorBuilder.ListenerStart() {
+                    @Override
+                    public void onAnimationStart() {
+
+                    }
+                })
+                .listenerEnd(new PathView.AnimatorBuilder.ListenerEnd() {
+
+                    private boolean isFirstIn;
+
+                    @Override
+                    public void onAnimationEnd() {
+                        pathView.setFillAfter(true);
+                        SharedPreferences sharedPreferences = getSharedPreferences("is_first_in_data", MODE_PRIVATE);
+                        isFirstIn = sharedPreferences.getBoolean("isFirstIn", true);
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (isFirstIn) {
+                                    startActivity(new Intent(WelcomeActivity.this, GuideActivity.class));
+                                    SharedPreferences pref = getSharedPreferences("is_first_in_data", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = pref.edit();
+                                    editor.putBoolean("isFirstIn", false);
+                                    editor.commit();
+                                } else {
+                                    startActivity(new Intent(WelcomeActivity.this, GuideActivity.class));
+                                    finish();
+                                }
+                            }
+                        }, 2000);
+                    }
+                })
+                .interpolator(new AccelerateDecelerateInterpolator())
+                .start();
+    }
+}
